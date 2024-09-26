26be84ec158320c0ba610af4714d1bbedb4e9507
tanku3080 <tankukuro@gmail.com>
2024-09-26 17:48:19 +0900

    画面遷移実装できず

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col">
           <h1>Real Time Chat System</h1>
           <Link
-            href="./pages/basicChatPage.tsx"
+            href={"./pages/bcp"}
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             target="_blank"
             rel="noopener noreferrer">
