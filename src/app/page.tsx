a22edcd971538e072c02508a2fad0e1ee76977e4
tanku3080 <tankukuro@gmail.com>
2024-09-27 10:00:42 +0900

    画面遷移が問題なく実施できるようにした。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,17 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col">
           <h1>Real Time Chat System</h1>
           <Link
-            href={"./pages/bcp"}
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            target="_blank"
-            rel="noopener noreferrer">
+          href={"pages/bcp"}
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          rel="noopener noreferrer">
             Basic Chat Start
           </Link>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link
+          href={"pages/pcp"}
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+          rel="noopener noreferrer">
             Private Chat Start
-          </a>
+          </Link>
         </div>
       </main>
     </div>
--- src/app/pages/bcp/page.tsx
+++ src/app/pages/bcp/page.tsx
@@ -1,9 +1,8 @@
-import { NextPage } from "next";
 import { initConfig } from "../../logic/initConfig";
 
 initConfig();
 
-const BasicChat:NextPage = () => {
+export default function BasicChat() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -30,5 +29,4 @@ const BasicChat:NextPage = () => {
       </div>
     );
   }
-  export default BasicChat
   
